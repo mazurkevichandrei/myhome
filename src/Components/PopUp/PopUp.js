803c951a519a7a4ddee0b3d9751ecdd803e0133d
Andrei Mazurkevich <mazurkevi4@gmail.com>
2023-03-21 13:29:01 +0300

    input_fields_1

--- src/Components/PopUp/PopUp.js
+++ src/Components/PopUp/PopUp.js
@@ -10,6 +10,7 @@ import StyledInput from "./Styled/StyledInput";
 
 const PopUp = ({isVisible,setPopUpVisible,calcWidth}) => {
 
+    //Pop Up Open Close rules START:
     const closePopUp = () => {
         setPopUpVisible(!isVisible)
     }
@@ -23,25 +24,49 @@ const PopUp = ({isVisible,setPopUpVisible,calcWidth}) => {
       }
       document.addEventListener('mousedown', handler)
     }, []); 
+    //Pop Up Open Close rules END:
+
+    //Change input values START:
+    const [homeNameRu, setHomeNameRu] = useState('')
+    const [homeNameEn, setHomeNameEn] = useState('')
+    const [address, setAddress] = useState('')
+    const [floorsCount, setFloorsCount] = useState('')
+    const [firstFloorsFlats, setFirstFloorsFlats] = useState('')
+    const [flatsPerFloor, setFlatsPerFloor] = useState('')
+    //Change input values END:
+    
+    //Generate House Data START:
+    const calcHouseData = () => {
+        const houseData = {
+            nameRu: homeNameRu,
+            homeNameEn: homeNameEn,
+            homeAddress: address,
+            houseFloorsCount: floorsCount,
+            firstFloor: firstFloorsFlats,
+            flatsOnFloor: flatsPerFloor
+        }
+        console.log(houseData)
+    }
+    //Generate House Data END:
 
     return(
         <StyledPopUp ref={ref2} isVisible={isVisible} calcWidth={calcWidth}>
             <StyledPopUpArea>
                 <StyledLable>House name Ru</StyledLable>
-                <StyledInput></StyledInput>
+                <StyledInput onChange={(e)=>{setHomeNameRu(e.target.value)}}></StyledInput>
                 <StyledLable>House name En</StyledLable>
-                <StyledInput></StyledInput>
+                <StyledInput onChange={(e)=>{setHomeNameEn(e.target.value)}}></StyledInput>
                 <StyledLable>Address</StyledLable>
-                <StyledInput></StyledInput>
+                <StyledInput onChange={(e)=>{setAddress(e.target.value)}}></StyledInput>
                 <StyledLable>Floors count</StyledLable>
-                <StyledInput></StyledInput>
+                <StyledInput onChange={(e)=>{setFloorsCount(e.target.value)}}></StyledInput>
                 <StyledLable>First floor flats count</StyledLable>
-                <StyledInput></StyledInput>
+                <StyledInput onChange={(e)=>{setFirstFloorsFlats(e.target.value)}}></StyledInput>
                 <StyledLable>Flats per floor</StyledLable>
-                <StyledInput></StyledInput>
+                <StyledInput onChange={(e)=>{setFlatsPerFloor(e.target.value)}}></StyledInput>
                 <StyledBtnArea>
                     <StyledCloseBtn onClick={()=>closePopUp()}>Cancel</StyledCloseBtn>
-                    <StyledCloseBtn isSave >Save</StyledCloseBtn>
+                    <StyledCloseBtn isSave onClick={calcHouseData} >Save</StyledCloseBtn>
                 </StyledBtnArea>
             </StyledPopUpArea>
         </StyledPopUp>
--- src/Components/PopUp/Styled/StyledInput.js
+++ src/Components/PopUp/Styled/StyledInput.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const StyledInput = styled.input`
     margin: 5px 0;
     margin-top: 2px;
-    border: 2px solid orange;
+    border: 1px solid grey;
     width: 100%;
     box-sizing: border-box;
     font-size: 16px;
